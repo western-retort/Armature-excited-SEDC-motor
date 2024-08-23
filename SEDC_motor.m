8e5211e1842288014d70d94edaa533c0dd72c33f
Ashhad SIddiqui <97237720+western-retort@users.noreply.github.com>
2024-08-22 20:54:29 -0700

    Add files via upload

---
+++ SEDC_motor.m
@@ -0,0 +1,11 @@
+Ra=0.5; %ohms
+B=1.3;
+J=5;%moment of inertia
+B=0.01;%frition viscous
+TL=20;%load torque
+tau_a=La/Ra;
+tau_m=J/B;%motor torque
+KePhi=1.6;
+Va=200;%torque voltage
+La=0.1;%inductance in henry
+
